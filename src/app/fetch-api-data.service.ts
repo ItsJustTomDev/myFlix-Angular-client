9ac724fed2edb73c5f4a18e14bcf4dbb2eaefeb2
ItsJustTomDev <96871598+ItsJustTomDev@users.noreply.github.com>
2022-07-15 16:40:52 +0300

    added login compoonent

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -8,7 +8,7 @@ import {
 import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-const apiUrl = 'https://secret-mesa-82091.herokuapp.com/';
+const apiUrl = 'https://marinanadj-53303.herokuapp.com/';
 
 @Injectable({
   providedIn: 'root',
